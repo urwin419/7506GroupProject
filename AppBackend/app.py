dbac126798ae61993703a328a306c76e34222fa7
LYZ-H <1025097693@qq.com>
2023-04-17 03:03:34 +0800

    initial login/register page

--- AppBackend/app.py
+++ AppBackend/app.py
@@ -69,5 +69,5 @@ def log_out():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
     CORS(app, supports_credentials=True)
